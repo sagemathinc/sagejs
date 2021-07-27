8588e8a4084aa6832e79ae2a4ddfb57716f5c30f
William Stein <wstein@sagemath.com>
2021-07-27 11:44:16 -0700

    some docker do not have platform flag

--- src/docker/build.sh
+++ src/docker/build.sh
@@ -2,4 +2,9 @@
 # supported by emscripten right now though this should change
 # soon, so even on arm (e.g., # apple silicon) we explicitly 
 # use amd64).
-sudo docker build  --platform linux/amd64 -t sagejs .
+
+if [[ `uname -m` == 'arm64' ]]; then
+  sudo docker build  --platform linux/amd64 -t sagejs .
+else
+  sudo docker build -t sagejs .
+fi
