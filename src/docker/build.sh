0a15d714ef4d3e04874007d26bd6a24ecb3a1bb1
William Stein <wstein@sagemath.com>
2021-07-27 11:41:35 -0700

    force x86 platform for builds, due to emscripten requirement right now

--- src/docker/build.sh
+++ src/docker/build.sh
@@ -1,1 +1,5 @@
-sudo docker build -t sagejs .
+# Note that only linux/amd64 as a build host is fully
+# supported by emscripten right now though this should change
+# soon, so even on arm (e.g., # apple silicon) we explicitly 
+# use amd64).
+sudo docker build  --platform linux/amd64 -t sagejs .
